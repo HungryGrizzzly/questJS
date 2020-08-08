988c03f0e9c1210e37a7dff203f503d80921a48b
StanMykola <mykola.stan@gmail.com>
2020-08-08 23:53:03 +0300

    feat: добавления четвертой и пятой задачи

--- jsStudy.js
+++ jsStudy.js
@@ -1,28 +1,33 @@
 // Первая задача
 
-/*function sumTo (n) { 
+/*
+function sumTo (n) { 
     var sum = 0;
         for(var i = 0; i<=n; i++){
             sum = sum + i; 
         }
     console.log(sum);
-}*/
+}
+*/
 
 //Вторая задача 
 
-/*for (var i = 0; i < 5; i++) {
+/*
+for (var i = 0; i < 5; i++) {
     setTimeout(function() {
         console.log(i);
     }(), 1000);
-}*/
+}
+*/
 
 // Третья задача
 
-/*var phrase;
+/*
+var phrase;
 function reverse (phrase) {
     var firstArray = [...phrase].reverse().join('');
-}*/
-
+}
+*/
 // Четвертая задача
 
 // Прочитал за каррирование, что стало для меня открытием, не понял как реализовать, 
@@ -30,4 +35,13 @@ function reverse (phrase) {
 
 // Пятая задача 
 
-
+/*
+function isPalindrome (phrase) {
+    var firstphrase = phrase;
+    var secondphrase = [...phrase].reverse().join('');
+    if (firstphrase == secondphrase)
+        console.log("true");
+    else
+    console.log("false");
+}
+*/
