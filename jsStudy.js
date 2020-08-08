9dcae668066cd1693a2e1655fe209a5e1fd677ea
StanMykola <mykola.stan@gmail.com>
2020-08-08 23:42:06 +0300

    fix: мелкие исправления первой задачи

--- jsStudy.js
+++ jsStudy.js
@@ -1,13 +1,12 @@
 // Первая задача
 
-/* var n = 0;
-var sum = 0;
-function sumTo(n) {
-    for(var i = 0; i<=n; i++){
-        sum = sum + i; 
-    }
-        console.log(sum);
-}*/ 
+/*function sumTo (n) { 
+    var sum = 0;
+        for(var i = 0; i<=n; i++){
+            sum = sum + i; 
+        }
+    console.log(sum);
+}*/
 
 //Вторая задача 
 
@@ -19,7 +18,16 @@ function sumTo(n) {
 
 // Третья задача
 
-var phrase;
+/*var phrase;
 function reverse (phrase) {
     var firstArray = [...phrase].reverse().join('');
-}
+}*/
+
+// Четвертая задача
+
+// Прочитал за каррирование, что стало для меня открытием, не понял как реализовать, 
+// но нашел информацию о методе _.curry с библиотеки lodash, которая делает это автоматически
+
+// Пятая задача 
+
+
