f0db836e62ae72652df7b05df14993d4ec4066c8
StanMykola <mykola.stan@gmail.com>
2020-08-08 22:54:47 +0300

    feat: добавления выполнения второй задачи

--- jsStudy.js
+++ jsStudy.js
@@ -1,8 +1,18 @@
-var n = 0;
+// Первая задача
+
+/* var n = 0;
 var sum = 0;
 function sumTo(n) {
     for(var i = 0; i<=n; i++){
         sum = sum + i; 
     }
         console.log(sum);
+}*/ 
+
+//Вторая задача 
+
+for (var i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }(), 1000);
 }
